a27f18ab8d5414cf7bf1fba87bd1e67dfcf9ffd1
monkonius <65208909+monkonius@users.noreply.github.com>
2023-04-05 17:49:16 +0800

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,23 @@
+import os
+import requests
+import json
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+def api_query(steamid):
+    try:
+        steamkey = os.getenv('STEAM_API_KEY')
+        url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/'
+        response = requests.get(f'{url}?key={steamkey}&steamid={steamid}&include_appinfo=true&include_played_free_games=true&format=json')
+        response.raise_for_status()
+
+    except requests.RequestException:
+        return None
+    
+    data = json.loads(response.text)
+    return data
