b3c3965581c35aaed05218db20584f6fcc90dbf3
monkonius <65208909+monkonius@users.noreply.github.com>
2023-04-10 17:00:39 +0800

    Add auth and database functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,38 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from .api import SECRET_KEY
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = SECRET_KEY
     app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db'
 
     from . import views
     app.register_blueprint(views.bp)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from .models import User
+
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
 
     return app
--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,63 @@
 from flask import Blueprint, render_template, request, redirect, flash, url_for
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db
+from .models import User
+from .api import get_player
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/login')
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        steamid = request.form.get('steamid')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(steamid=steamid).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in!')
+                login_user(user, remember=True)
+                return redirect(url_for('views.index'))
+            else:
+                flash('Password is incorrect', 'error')
+        else:
+            flash('That user does not exist', 'error')
+
     return render_template('login.html')
 
 
-@bp.route('/register')
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        steamid = request.form.get('steamid')
+        password = request.form.get('password')
+        confirm = request.form.get('confirm')
+
+        if not get_player(steamid):
+            flash('Invalid Steam ID', 'error')
+
+        user = User.query.filter_by(steamid=steamid).first()
+        if user:
+            flash('Steam ID is already in use', 'error')
+        elif password != confirm:
+            flash('Passwords do not match', 'error')
+        else:
+            new_user = User(steamid=steamid, password=generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+
+            login_user(new_user, remember=True)
+            flash('Registration successful!')
+            return redirect(url_for('views.index'))
+
     return render_template('register.html')
+
+
+@bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
