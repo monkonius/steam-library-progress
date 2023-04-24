fc699bffe41ca059e02837253a39137378bb3bd7
monkonius <65208909+monkonius@users.noreply.github.com>
2023-04-24 18:14:13 +0800

    Use enum for the todo state column since data is fixed

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
+import enum
 from . import db
 from flask_login import UserMixin
+
+
+class StateEnum(enum.Enum):
+    PLAYING = 'playing'
+    FINISHED = 'finished'
+    ON_HOLD = 'on hold'
+    DROPPED = 'dropped'
+    TO_PLAY = 'to play'
 
 
 class User(db.Model, UserMixin):
@@ -15,7 +24,8 @@ class User(db.Model, UserMixin):
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     game = db.Column(db.String(400))
-    state = db.Column(db.String(8), default='to play')
+    state = db.Column(db.Enum(StateEnum, values_callable=lambda x: [
+                                 str(member.value) for member in x]), default=StateEnum.TO_PLAY)
     player = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete='CASCADE'), nullable=False)
     
