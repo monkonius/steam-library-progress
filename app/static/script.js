ed07477391007446d3b1bf538a3b610175bcb12b
monkonius <65208909+monkonius@users.noreply.github.com>
2023-04-29 16:41:22 +0800

    Add dropdown menu when logged in

---
+++ app/static/script.js
@@ -0,0 +1,13 @@
+function showContent(button) {
+    dropdownContent = button.nextElementSibling;
+    dropdownContent.classList.toggle('show');
+}
+
+window.onclick = (event) => {
+    if (!event.target.matches('.dropdown')) {
+        const dropdownContent = document.querySelector('.dropdown-content');
+        if (dropdownContent.classList.contains('show')) {
+            dropdownContent.classList.remove('show');
+        }
+    }
+}
